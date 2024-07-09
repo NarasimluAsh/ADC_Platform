34235d7e4fdfedc6fa40e5cea7205bf28fc454fd
Gwen <daphne.paquiom1@umail.uom.ac.mu>
2024-07-09 15:54:23 +0400

    Getting students

--- src/pages/ClassroomPage.jsx
+++ src/pages/ClassroomPage.jsx
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { db } from '../firebase';
 import { useNavigate } from 'react-router-dom';
 
+
 const ClassroomPage = () => {
+
+    const [students, setStudents] = useState([]);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        // Fetch students from Firestore
+        const fetchStudents = async () => {
+            const studentsRef = db.collection('IEPs');
+            const snapshot = await studentsRef.get();
+            if (snapshot.empty) {
+                console.log('No matching documents.');
+                return;
+            }
+
+            let studentsList = [];
+            snapshot.forEach(doc => {
+                studentsList.push(doc.data().name); // Assuming 'name' is a field in your student documents
+            });
+
+            setStudents(studentsList);
+        };
+
+        fetchStudents();
+    }, []);
+
     return (
         <div className="Classroom-container">
             <h1>Classrooms</h1>
+            <ul>
+                {students.map((student, index) => (
+                    <li key={index}>{student}</li>
+                ))}
+            </ul>
             <button
                 className="add-student-page"
                 onClick={() => navigate('/add-student')}
             >Add Student IEP</button>
-    </div>
+        </div>
     );
 };
 
